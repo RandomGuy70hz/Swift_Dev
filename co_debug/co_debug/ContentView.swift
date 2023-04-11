77c6d32ac98958d5eb7f9a9cd3d58a0c8d063af7
RandomGuy <codymoores@live.com>
2023-04-11 05:40:48 -0230

    updates

--- co_debug/co_debug/ContentView.swift
+++ co_debug/co_debug/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created on 2023-04-09.
 //
-
+/*
 import SwiftUI
 
 struct ContentView: View {
@@ -99,5 +99,115 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}*/
+
+
+
+import SwiftUI // Import the SwiftUI framework
+
+struct ContentView: View { // Define the ContentView struct which conforms to the View protocol
+    // Create state variables for input values
+    @State private var hourlyRate: String = ""
+    @State private var regularHours: String = ""
+    @State private var regularOvertimeHours: String = "" // Regular overtime hours input
+    @State private var holidayOvertimeHours: String = "" // Holiday overtime hours input
+    @State private var workedStatHoliday: Bool = false // Statutory holiday toggle state
+    @State private var regularOvertimeRate: String = "" // Regular overtime rate input
+    @State private var holidayOvertimeRate: String = "" // Holiday overtime rate input
+    @State private var taxRate: String = "" // Tax rate input
+    
+    // Add toggle for regular overtime
+    @State private var regularOvertime: Bool = false // Regular overtime toggle state
+    
+    @State private var showSummary: Bool = false // Summary sheet presentation state
+    
+    // Define the user interface for ContentView
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Hourly Rate")) {
+                    TextField("Enter your hourly rate ($)", text: $hourlyRate)
+                        .keyboardType(.decimalPad) // Use decimal keypad
+                }
+                
+                Section(header: Text("Regular Hours")) {
+                    TextField("Enter your regular worked hours", text: $regularHours)
+                        .keyboardType(.decimalPad) // Use decimal keypad
+                }
+                
+                // Regular Overtime Toggle
+                Section {
+                    Toggle("Overtime", isOn: $regularOvertime) // Display overtime toggle
+                    
+                    if regularOvertime {
+                        TextField("Enter # of hours", text: $regularOvertimeHours)
+                            .keyboardType(.decimalPad) // Use decimal keypad
+                    }
+                }
+                
+                Section {
+                    Toggle("Statutory Holiday", isOn: $workedStatHoliday) // Display statutory holiday toggle
+                    
+                    if workedStatHoliday {
+                        TextField("Enter number # of hours", text: $holidayOvertimeHours)
+                            .keyboardType(.decimalPad) // Use decimal keypad
+                    }
+                }
+                
+                /*Section(header: Text("Overtime Rates")) {
+                    if regularOvertime {
+                        TextField("Enter overtime rate", text: $regularOvertimeRate)
+                            .keyboardType(.decimalPad) // Use decimal keypad
+                    }
+                    
+                    if workedStatHoliday {
+                        TextField("Enter overtime rate for statutory holiday", text: $holidayOvertimeRate)
+                            .keyboardType(.decimalPad) // Use decimal keypad
+                    }
+                }*/
+                
+                // Overtime Rates section with conditional visibility
+                if regularOvertime || workedStatHoliday {
+                    Section(header: Text("Overtime Rates")) {
+                        if regularOvertime {
+                            TextField("Enter overtime rate", text: $regularOvertimeRate)
+                                .keyboardType(.decimalPad) // Use decimal keypad
+                        }
+                        
+                        if workedStatHoliday {
+                            TextField("Enter overtime rate for statutory holiday", text: $holidayOvertimeRate)
+                                .keyboardType(.decimalPad) // Use decimal keypad
+                        }
+                    }
+                }
+
+                
+                // User input tax
+                Section(header: Text("Tax Rate")) {
+                    TextField("Enter your tax rate (%)", text: $taxRate)
+                        .keyboardType(.numberPad) // Use number keypad
+                } // End of tax section
+                
+                
+                // Calculate button
+                Button(action: {
+                    self.showSummary = true // Set showSummary to true to display summary sheet
+                }) {
+                    Text("Calculate")
+                        .frame(minWidth: 0, maxWidth: .infinity) // Make button full width
+                }
+            }
+            .navigationTitle("Paycheque Estimator") // Set navigation title
+        }
+        .sheet(isPresented: $showSummary) { // Show the summary sheet when showSummary is true
+            SummaryView(hourlyRate: self.$hourlyRate, regularHours: self.$regularHours, regularOvertimeHours: self.$regularOvertimeHours, holidayOvertimeHours: self.$holidayOvertimeHours, workedStatHoliday: self.$workedStatHoliday, regularOvertimeRate: self.$regularOvertimeRate, holidayOvertimeRate: self.$holidayOvertimeRate, regularOvertime: self.$regularOvertime, taxRate: self.$taxRate)
+        }
+    }
+}
+    
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView() // Instantiate ContentView for preview
     }
 }
--- co_debug/co_debug/SummaryView.swift
+++ co_debug/co_debug/SummaryView.swift
@@ -4,7 +4,7 @@
 //
 //  Created on 2023-04-09.
 //
-
+/*
 import SwiftUI
 
 struct SummaryView: View {
@@ -112,6 +112,122 @@ struct SummaryView_Previews: PreviewProvider {
         SummaryView(hourlyRate: .constant("25"), regularHours: .constant("40"), regularOvertimeHours: .constant("5"), holidayOvertimeHours: .constant("0"), workedStatHoliday: .constant(false), regularOvertimeRate: .constant("1.5"), holidayOvertimeRate: .constant("2"), regularOvertime: .constant(true), taxRate: .constant("25"))
     }
 
+}*/
+
+import SwiftUI // Import the SwiftUI framework
+
+struct SummaryView: View { // Define the SummaryView struct which conforms to the View protocol
+    @Environment(\.presentationMode) var presentationMode // Access presentation mode to dismiss the view
+    
+    // Bindings for each input value from ContentView
+    @Binding var hourlyRate: String // User input for hourly rate
+    @Binding var regularHours: String // User input for regular hours
+    @Binding var regularOvertimeHours: String // User input for regular overtime hours
+    @Binding var holidayOvertimeHours: String // User input for holiday overtime hours
+    @Binding var workedStatHoliday: Bool // User input for whether a stat holiday was worked
+    @Binding var regularOvertimeRate: String // User input for regular overtime rate
+    @Binding var holidayOvertimeRate: String // User input for holiday overtime rate
+    @Binding var regularOvertime: Bool // User input for whether regular overtime was worked
+    @Binding var taxRate: String // User input for tax rate
+    @State private var showSummary: Bool = false // State variable to control showing summary
+
+    // Function to clear all input fields
+    func clearFields() {
+        hourlyRate = "" // Clear hourly rate field
+        regularHours = "" // Clear regular hours field
+        regularOvertimeHours = "" // Clear regular overtime hours field
+        holidayOvertimeHours = "" // Clear holiday overtime hours field
+        workedStatHoliday = false // Reset workedStatHoliday to false
+        regularOvertimeRate = "" // Clear regular overtime rate field
+        holidayOvertimeRate = "" // Clear holiday overtime rate field
+        taxRate = "" // Clear tax rate field
+    }
+    
+    // Calculate total hours worked, including regular, regular overtime, and holiday overtime
+    var totalHours: Double {
+        let regHours = Double(regularHours) ?? 0 // Convert regularHours to Double or default to 0
+        let regOvertimeHours = Double(regularOvertimeHours) ?? 0 // Convert regularOvertimeHours to Double or default to 0
+        let regOvertimeRate = Double(regularOvertimeRate) ?? 0 // Convert regularOvertimeRate to Double or default to 0
+        let holOvertimeHours = Double(holidayOvertimeHours) ?? 0 // Convert holidayOvertimeHours to Double or default to 0
+        let holOvertimeRate = Double(holidayOvertimeRate) ?? 0 // Convert holidayOvertimeRate to Double or default to 0
+        
+        // Calculate total hours by adding regular hours, regular overtime, and holiday overtime
+        return regHours + (regOvertimeHours * regOvertimeRate) + (holOvertimeHours * holOvertimeRate)
+    }
+    
+    // Calculate gross pay based on hours worked and rates
+    var grossPay: Double {
+        let rate = Double(hourlyRate) ?? 0 // Convert hourlyRate to Double or default to 0
+        let regHours = Double(regularHours) ?? 0 // Convert regularHours to Double or default to 0
+        let regOvertimeHours = Double(regularOvertimeHours) ?? 0 // Convert regularOvertimeHours to Double or default to 0
+        let regOvertimeRate = Double(regularOvertimeRate) ?? 0 // Convert regularOvertimeRate to Double or default to 0
+        let holOvertimeHours = Double(holidayOvertimeHours) ?? 0 // Convert holidayOvertimeHours to Double or default to 0
+        let holOvertimeRate = Double(holidayOvertimeRate) ?? 0 // Convert holidayOvertimeRate to Double or default to 0
+        
+        // Calculate gross pay by adding regular pay, regular overtime pay, and holiday overtime pay
+        return (rate * regHours) +
+            (rate * regOvertimeHours * regOvertimeRate) +
+            (rate * holOvertimeHours * holOvertimeRate)
+    }
+
+    // Calculate the estimated tax based on the tax rate and gross pay
+    var estimatedTax: Double {
+        let rate = Double(taxRate) ?? 0 // Convert taxRate to Double or default to 0
+        return grossPay * (rate / 100) // Calculate tax by multiplying gross pay by tax rate percentage
+    }
+    
+    // Calculate net pay by subtracting estimated tax from gross pay
+    var netPay: Double {
+        return grossPay - estimatedTax
+    }
+    
+    // Define the user interface for SummaryView
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Summary") // Display "Summary" title
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Hourly rate: $\(hourlyRate)") // Display hourly rate
+                Text("Total regular hours: \(regularHours) hr(s)") // Display total regular hours
+                if regularOvertime {
+                    Text("Total regular overtime hours: \(regularOvertimeHours) hr(s)") // Display total regular overtime hours
+                }
+                if workedStatHoliday {
+                    Text("Total holiday overtime hours: \(holidayOvertimeHours) hr(s)") // Display total holiday overtime hours
+                }
+                Text("Total hours: \(totalHours, specifier: "%.2f") hr(s)") // Display total hours
+                Text("Expected gross pay: $\(grossPay, specifier: "%.2f")") // Display gross pay
+                Text("Estimated tax: $\(estimatedTax, specifier: "%.2f")") // Display estimated tax
+                Text("Net pay: $\(netPay, specifier: "%.2f")") // Display net pay
+
+                // Add a clear button to clear all fields
+                Button("Clear") {
+                    clearFields() // Call the clearFields function
+                    showSummary = false // Hide the summary
+                    presentationMode.wrappedValue.dismiss() // Dismiss the view
+                }
+                .padding()
+            }
+            .padding()
+        }
+        
+        // Add a "Done" button to close the view
+        Button("Done") {
+            presentationMode.wrappedValue.dismiss() // Dismiss the view
+        }
+        .padding() // Add padding to the "Done" button
+    }
 }
+
+// Define the preview for SummaryView
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView(hourlyRate: .constant("25"), regularHours: .constant("40"), regularOvertimeHours: .constant("5"), holidayOvertimeHours: .constant("0"), workedStatHoliday: .constant(false), regularOvertimeRate: .constant("1.5"), holidayOvertimeRate: .constant("2"), regularOvertime: .constant(true), taxRate: .constant("25"))
+    }
+}
+
+
 
 
